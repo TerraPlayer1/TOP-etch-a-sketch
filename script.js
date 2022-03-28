145f4f2a7d407c12dff80f673ff5d7782dab805f
TerraPlayer1 <terragamer70@gmail.com>
2022-03-28 19:36:02 +0300

    Add resizable grid for drawing

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
+const container = document.querySelector(".container")  // Html imports
 const drawingArea = document.querySelector(".drawing-area");
+const slider = document.getElementById("myRange")
+const output = document.getElementById("demo");
 
-let a = 0
-while (a < 32) {
-  const addDiv =  document.createElement("div");
-  drawingArea.appendChild(addDiv);
-  a += 1
+output.innerHTML = slider.value+ " * " + slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value + " * " + this.value;
+}
+
+// Update the current drawingArea
+slider.onmouseup = function() {
+  delRows();
+  makeRows(this.value, this.value);
+} 
+
+
+function makeRows(rows, cols) {
+  drawingArea.style.setProperty('--grid-rows', rows); // Update root css values
+  drawingArea.style.setProperty('--grid-cols', cols);
+  for (c = 0; c < (rows * cols); c++) {         // Create grid
+    let cell = document.createElement("div");
+    drawingArea.appendChild(cell).className = "grid-item";
+  };
 };
 
+
+function delRows() {
+  while (drawingArea.hasChildNodes()){
+    drawingArea.removeChild(drawingArea.firstChild);
+  }
+  container.appendChild(drawingArea);
+};
+
+
+makeRows(slider.value, slider.value);

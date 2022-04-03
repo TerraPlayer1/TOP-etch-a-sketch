edbd729390863c717819cce205523ee154fae0e3
TerraPlayer1 <terragamer70@gmail.com>
2022-04-03 11:44:54 +0300

    Draw when holding mouse button down

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const container = document.querySelector(".container")  // Html imports
+const container = document.querySelector(".container");  // Html imports
 const drawingArea = document.querySelector(".drawing-area");
 const slider = document.getElementById("myRange")
 const output = document.getElementById("demo");
@@ -11,21 +11,41 @@ slider.oninput = function() {
 }
 
 // Update the current drawingArea
-slider.onmouseup = function() {
+slider.onchange = function() {
   delRows();
   makeRows(this.value, this.value);
 } 
 
 
+let draw = false;
+let color = "black";
+//Build the rows for the drawing area
 function makeRows(rows, cols) {
   drawingArea.style.setProperty('--grid-rows', rows); // Update root css values
   drawingArea.style.setProperty('--grid-cols', cols);
   for (c = 0; c < (rows * cols); c++) {         // Create grid
     let cell = document.createElement("div");
     drawingArea.appendChild(cell).className = "grid-item";
-  };
+
+    
+    cell.addEventListener('mouseover', function(){
+      if(!draw) return
+      cell.style.backgroundColor = color;
+  })
+  cell.addEventListener('mousedown', function(){
+      cell.style.backgroundColor = color;
+  })
+
+  drawingArea.appendChild(cell)
+};
 };
 
+window.addEventListener("mousedown", function(){
+  draw = true
+})
+window.addEventListener("mouseup", function(){
+  draw = false
+})
 
 function delRows() {
   while (drawingArea.hasChildNodes()){
@@ -33,6 +53,5 @@ function delRows() {
   }
   container.appendChild(drawingArea);
 };
-
 
 makeRows(slider.value, slider.value);

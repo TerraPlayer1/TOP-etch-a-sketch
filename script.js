f048e4234d04b3e1a9b3f5b5b86738fa1d670327
TerraPlayer1 <terragamer70@gmail.com>
2022-04-08 19:34:57 +0300

    Discard if else loops for a more elegant solution

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ function makeGrid(rows, cols) {
     drawingArea.appendChild(cell).className = "grid-item";
     cell.classList.add("100%");
     cell.style.backgroundColor = "rgb(255,255,255)"
+    cell.style.filter = "brightness(100%)"
 }};
 
 function resetGrid() {
@@ -105,10 +106,12 @@ let rgbHover = function() {
   if(!draw) return
   let randomColor = Math.floor(Math.random()*16777215).toString(16);
   this.style.backgroundColor = "#" + randomColor;
+  this.style.filter = "brightness(100%)"
 };
 let rgbClick = function() {
   let randomColor = Math.floor(Math.random()*16777215).toString(16);
   this.style.backgroundColor = "#" + randomColor;
+  this.style.filter = "brightness(100%)"
 };
 function drawRGB() {  // To do: Remove black from color pool
   for (c = 0; c < grid.length; c++) {
@@ -128,127 +131,21 @@ let darkHover = function(){
 let darkClick = function(){
   increment(this);
 };
-let darkRgbHover = function(){
-  if(!draw) return
-  rgbIncrement(this);
-};
-let darkRgbClick = function(){
-  rgbIncrement(this);
-};
-
 function drawDark() {
   for (c = 0; c < grid.length; c++) {
-    if (grid[c].style.backgroundColor != "rgb(0,0,0)" && grid[c].style.backgroundColor != "rgb(255,255,255)"){
-    grid[c].removeEventListener('mouseover',darkRgbHover);
-    grid[c].addEventListener('mouseover',darkRgbHover);
+    grid[c].removeEventListener('mouseover',darkHover);
+    grid[c].addEventListener('mouseover',darkHover);
 
-    grid[c].removeEventListener('mousedown',darkRgbClick);
-    grid[c].addEventListener('mousedown',darkRgbClick);
-  } else {    
-    for (c = 0; c < grid.length; c++) {
-      grid[c].removeEventListener('mouseover',darkHover);
-      grid[c].addEventListener('mouseover',darkHover);
-
-      grid[c].removeEventListener('mousedown',darkClick);
-      grid[c].addEventListener('mousedown',darkClick);
-      };
-    };
+    grid[c].removeEventListener('mousedown',darkClick);
+    grid[c].addEventListener('mousedown',darkClick);
   };
 };
 
-
-
 function increment(item) {
   if (mode === "dark"){
-    if (item.classList.contains("100%")) {
-      item.style.backgroundColor = `rgba(0,0,0,${0.1})`
-      item.classList.remove("100%");
-      item.classList.add("90%");
-    } else if (item.classList.contains("90%")){
-      item.style.backgroundColor = `rgba(0,0,0,${0.2})`
-      item.classList.remove("90%");
-      item.classList.add("80%");
-    } else if (item.classList.contains("80%")){
-      item.style.backgroundColor = `rgba(0,0,0,${0.3})`
-      item.classList.remove("80%");
-      item.classList.add("70%");
-    } else if (item.classList.contains("70%")){
-      item.style.backgroundColor = `rgba(0,0,0,${0.4})`
-      item.classList.remove("70%");
-      item.classList.add("60%");
-    } else if (item.classList.contains("60%")){
-      item.style.backgroundColor = `rgba(0,0,0,${0.5})`
-      item.classList.remove("60%");
-      item.classList.add("50%");
-    } else if (item.classList.contains("50%")){
-      item.style.backgroundColor = `rgba(0,0,0,${0.6})`
-      item.classList.remove("50%");
-      item.classList.add("40%");
-    } else if (item.classList.contains("40%")){
-      item.style.backgroundColor = `rgba(0,0,0,${0.7})`
-      item.classList.remove("40%");
-      item.classList.add("30%");
-    } else if (item.classList.contains("30%")){
-      item.style.backgroundColor = `rgba(0,0,0,${0.8})`
-      item.classList.remove("30%");
-      item.classList.add("20%");
-    } else if (item.classList.contains("20%")){
-      item.style.backgroundColor = `rgba(0,0,0,${0.9})`
-      item.classList.remove("20%");
-      item.classList.add("10%");
-    } else if (item.classList.contains("10%")){
-      item.style.backgroundColor = `rgba(0,0,0,${1})`
-      item.classList.remove("10%");
-      item.classList.add("0%");
-    } else item.style.backgroundColor = `rgba(0,0,0,${1})`;
-  } else item.classList.add("100%");
-  
-};
-function rgbIncrement(item) {
-
-  if (mode === "dark"){
-    if (item.classList.contains("100%")) {
-      item.style.filter = "brightness(90%)"
-      item.classList.remove("100%");
-      item.classList.add("90%");
-    } else if (item.classList.contains("90%")){
-      item.style.filter = "brightness(80%)"
-      item.classList.remove("90%");
-      item.classList.add("80%");
-    } else if (item.classList.contains("80%")){
-      item.style.filter = "brightness(70%)"
-      item.classList.remove("80%");
-      item.classList.add("70%");
-    } else if (item.classList.contains("70%")){
-      item.style.filter = "brightness(60%)"
-      item.classList.remove("70%");
-      item.classList.add("60%");
-    } else if (item.classList.contains("60%")){
-      item.style.filter = "brightness(50%)"
-      item.classList.remove("60%");
-      item.classList.add("50%");
-    } else if (item.classList.contains("50%")){
-      item.style.filter = "brightness(40%)"
-      item.classList.remove("50%");
-      item.classList.add("40%");
-    } else if (item.classList.contains("40%")){
-      item.style.filter = "brightness(30%)"
-      item.classList.remove("40%");
-      item.classList.add("30%");
-    } else if (item.classList.contains("30%")){
-      item.style.filter = "brightness(20%)"
-      item.classList.remove("30%");
-      item.classList.add("20%");
-    } else if (item.classList.contains("20%")){
-      item.style.filter = "brightness(10%)"
-      item.classList.remove("20%");
-      item.classList.add("10%");
-    } else if (item.classList.contains("10%")){
-      item.style.filter = "brightness(0%)"
-      item.classList.remove("10%");
-      item.classList.add("0%");
-    } else item.style.filter = "brightness(0%)"
-  } else item.classList.add("100%");
+  let val = parseFloat(item.style.filter.replace(/[^-\d\.]/g, ''))
+  item.style.filter = `brightness(${val-10}%)`
+  } else item.style.filter = "brightness(100%)"
 };
 
 window.addEventListener("mousedown", function(){
